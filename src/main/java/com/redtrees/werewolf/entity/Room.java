410dd80a35126afbc9392a2792cc2e2d09389654
redtrees <1974922572@qq.com>
2020-10-18 22:04:14 +0800

    init

--- src/main/java/com/redtrees/werewolf/entity/Room.java
+++ src/main/java/com/redtrees/werewolf/entity/Room.java
@@ -1,10 +1,14 @@
 package com.redtrees.werewolf.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.List;
-
+@Entity
 public class Room {
+    @Id
+    private int id;
     private int roomid;
-    private List<Player> playerList;
+    private String playerList;
     private String gameStart;
     private int curPlayerNo;
     private String gameMode;
